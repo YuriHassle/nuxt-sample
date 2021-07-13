a3aacf4e63cc87abb4cdc300f4be0c67fa88dc42
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-07-13 03:34:23 -0400

    feat: add Nav component to default layout

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <Nav />
     <div v-if="product" class="container py-5">
       <div class="hero-container">
         <img
@@ -50,7 +49,7 @@
       </div>
     </div>
     <div v-else class="container padding">
-      Page Not Found
+      <PageNotFound />
     </div>
   </div>
 </template>
