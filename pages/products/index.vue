7c6d0aa3941e2e91349c98d811c1ccaf8d0e3892
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-07-13 02:07:45 -0400

    feat: create products page

---
+++ pages/products/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <Nav />
+    <div class="container">
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo.slice(0, 1)"
+        :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardInfo"
+        :key="cardInfo.id"
+        :cardSection="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from "@/assets/data";
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardInfo: smallCardSections
+    };
+  }
+};
+</script>
+
+<style scoped></style>
