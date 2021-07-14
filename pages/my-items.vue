5522d14f117a598db9f06a5c6bf05bd2e9a95c7e
Yuri Araújo <yuri.hassle.araujo@gmail.com>
2021-07-14 01:43:47 -0400

    feat: add my-items page

--- pages/my-items.vue
+++ pages/my-items.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>My items</div>
+  <div class="container">
+    <MyItem v-for="item in myItems" :key="item.id" :item="item" />
+  </div>
 </template>
 
 <script>
 export default {
-  layout: "no-nav"
+  layout: "no-nav",
+  data() {
+    return {
+      myItems: this.$store.state.myRentals
+    };
+  }
 };
 </script>
 
